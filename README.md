fa1dce82ce03a855a41982db7ab9f232ad40caff
Zijie Jiang <1017184242@qq.com>
2024-10-22 14:32:40 +0800

    refactor airflow

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Airflow based on Python venv
+## Airflow based on Python3.9.6 venv
 
-1. Create a virtual environment.
+1. Create a virtual environment in the current path.
 ```bash
 python3 -m venv my-venv
 ```
@@ -8,19 +8,30 @@ python3 -m venv my-venv
 2. Activate the virtual environment.
 ```bash
 source my-venv/bin/activate
+
+export AIRFLOW_HOME=$(pwd)/airflow
 ```
 
-3. Install the required packages.
+3. Setup the airflow environment variables and install it
+```shell
+AIRFLOW_VERSION=2.10.2
+
+PYTHON_VERSION="$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
+
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+
+pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+```
+
+4. Install the required packages.
+
 ```bash
 pip3 install pyspark
-```
-```bash
-pip3 install apache-airflow
-```
-> ETL coding and then add the dags file(`airflow_dag_demo.py`) to the `dags` folder(~/airflow/dags)
-> Ensure the dags file can be executed at the airflow path.
 
-4. Start the Airflow web server.
+pip3 install pandas
+```
+5. Start the Airflow web server.
+
 ```bash
 airflow standalone
 ```
@@ -28,4 +39,20 @@ airflow standalone
 ## Tips
 
 [Data source](https://www.backblaze.com/cloud-storage/resources/hard-drive-test-data)
+
+Exit python venv with the `deactivate` command
+
+## TroubleShooting
+
+SSL: DECRYPTION_FAILED_OR_BAD_RECORD_MAC
+
+```shell
+pip3 install urllib3 --upgrade
+```
+
+java.lang.ClassNotFoundException: com.mysql.cj.jdbc.Driver
+
+```
+add mysql jar in my-venv/lib/python3.9/site-packages/pyspark/jars directory
+```
 
---
+++ airflow/dags/dag_demo.py
@@ -0,0 +1,81 @@
+import io
+import zipfile
+
+import pandas
+import pendulum
+import requests
+from airflow.decorators import task, dag
+from pyspark.sql import functions, SparkSession
+from pyspark.sql.functions import year, udf
+from pyspark.sql.types import StringType
+
+API = "https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q3_2023.zip"
+base = "./airflow/files"
+
+url = 'jdbc:mysql://localhost:13306/pyspark?characterEncoding=UTF-8'
+mode = 'overwrite'  # append
+prop = {'user': 'root', 'password': '123456', 'driver': 'com.mysql.cj.jdbc.Driver'}
+
+model_map = {
+    'CT': 'Crucial',
+    'DELLBOSS': 'Dell BOSS',
+    'HGST': 'HGST',
+    'Seagate': 'Seagate',
+    'ST': 'Seagate',
+    'TOSHIBA': 'Toshiba',
+    'WDC': 'Western Digital'
+}
+
+
+@udf(returnType=StringType())
+def get_brand(model):
+    for key in model_map.keys():
+        if model.startswith(key):
+            return model_map[key]
+    return 'Others'
+
+
+@dag(
+    dag_id="etl",
+    schedule=None,
+    start_date=pendulum.datetime(2024, 10, 17),
+    catchup=False,
+    tags=["dag_demo"],
+)
+def etl():
+    spark = SparkSession.builder.appName("dag_demo").getOrCreate()
+
+    @task
+    def extract():
+        res = requests.get(API)
+        zip_file = zipfile.ZipFile(io.BytesIO(res.content))
+        zip_file.extractall(base)
+        return base + '/data_Q3_2023'
+
+    @task
+    def transfer1(_path: str):
+        data = spark.read.csv(_path, header=True, inferSchema=True)
+        data = data.groupBy("date").agg(functions.count("*").alias("count"), functions.sum("failure").alias("failures"))
+        return data.toPandas().to_json()
+
+    @task
+    def transfer2(_path: str):
+        data = spark.read.csv(_path, header=True, inferSchema=True)
+        data = (data.groupBy(year("date").alias("year"), get_brand("model").alias("brand"))
+                .agg(functions.sum("failure").alias("failures")))
+        return data.toPandas().to_json()
+
+    @task
+    def load(json_data: str, _tbl_name: str):
+        data = spark.createDataFrame(pandas.read_json(json_data))
+        data.write.jdbc(url, _tbl_name, mode, prop)
+
+    _path = extract()
+    load(transfer1(_path), 'data_Q3_2023')
+    load(transfer2(_path), 'data_Q3_2023_2')
+
+
+try:
+    etl()
+except Exception as e:
+    print(e)
